88b5a758513e5fd6f934e1912684e43ac7740c14
yukijina <yukijina@gmail.com>
2020-11-27 13:41:28 -0800

    create table

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,27 @@
 import React, { useState } from 'react';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import AddIcon from '@material-ui/icons/Add';
 import Switch from '@material-ui/core/Switch';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Table from '@material-ui/core/Table';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableBody from '@material-ui/core/TableBody';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import Paper from '@material-ui/core/Paper';
+import FilterListIcon from '@material-ui/icons/FilterList';
 
 const useStyles = makeStyles(theme => ({}));
+
+function createData(name, date, service, features, complexity, platforms, users, total) {
+  return { name, date, service ,features, complexity, platforms, users, total}
+}
 
 export default function ProjectManager() {
   const classes = useStyles();
@@ -19,8 +31,8 @@ export default function ProjectManager() {
   const [iOSChecked, setIOSChecked] = useState(false);
   const [androidChecked, setAndroidChecked] = useState(false);
   const [softwareChecked, setSoftwareChecked] = useState(false);
-
-
+  const [rows, setRows] = useState([createData("Tom", "11/2/19", "Websote", "E-commerce", "N/A", "N/A", "N/A", "$1500")])
+  
   return (
     <Grid container direction="column">
       <Grid item style={{ marginTop: "2em", marginLeft: "5em"}}>
@@ -33,7 +45,7 @@ export default function ProjectManager() {
           InputProps={{
             endAdornment: (
               <InputAdornment position="end">
-                <AddIcon color="primary" />
+                <AddIcon color="primary" style={{ fontSize: 30 }} />
               </InputAdornment>
              )
           }}
@@ -86,6 +98,56 @@ export default function ProjectManager() {
             labelPlacement="start"
           />
         </FormGroup>
+      </Grid>
+
+      <Grid item container justify="flex-end" style={{ marginTop: "5em" }}>
+        <Grid item style={{ marginRight: 75 }}>
+          <FilterListIcon color="secondary" style={{ fontSize: 50 }} />
+        </Grid>
+      </Grid>
+      <Grid item>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  Name
+                </TableCell>
+                <TableCell>
+                  Date
+                </TableCell>
+                <TableCell>
+                  Service
+                </TableCell>
+                <TableCell>
+                  Features
+                </TableCell>
+                <TableCell>
+                  Complexity
+                </TableCell>
+                <TableCell>
+                  Platforms
+                </TableCell>
+                <TableCell>
+                  Total
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row, index) => 
+              <TableRow key={index}>
+                <TableCell>{row.name}</TableCell>
+                <TableCell>{row.date}</TableCell>
+                <TableCell>{row.service}</TableCell>
+                <TableCell>{row.features}</TableCell>
+                <TableCell>{row.complexity}</TableCell>
+                <TableCell>{row.platforms}</TableCell>
+                <TableCell>{row.users}</TableCell>
+                <TableCell>{row.total}</TableCell>
+              </TableRow>)}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Grid>
     </Grid>
   )
